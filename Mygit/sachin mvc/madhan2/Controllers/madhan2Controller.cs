57cac8a06ae01a0793576d8a767e8d0e2899855b
priya-project <priyabala032@gmail.com>
2019-11-07 11:59:10 +0530

    message

---
+++ Mygit/sachin mvc/madhan2/Controllers/madhan2Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using madhan2.Models;
+
+namespace madhan2.Controllers
+{
+    public class madhan2Controller : Controller
+    {
+        // GET: madhan2
+        public ActionResult HOME()
+        {
+            
+            return View();
+        }
+        public ActionResult ABOUT()
+        {
+            
+            return View();
+        }
+        public ActionResult ADDRESS()
+        {
+            
+            return View();
+        }
+        public ActionResult ACHIVE()
+        {
+            
+            return View();
+        }
+        public ActionResult DATA(Madhan obj)
+        {
+            madhanEntities db = new madhanEntities();
+            db.Madhans.Add(obj);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ Mygit/sachin mvc/madhan2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(madhan2.Startup))]
+namespace madhan2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
